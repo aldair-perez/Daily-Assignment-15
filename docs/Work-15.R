cdae237b5443d857c79a15435a16cb3ca61e027c
Aldair Perez <aldairperez864@gmail.com>
2025-04-02 11:10:53 -0600

    Add files via upload

---
+++ docs/Work-15.R
@@ -0,0 +1,21 @@
+# Part B
+install.packages("tidymodels")
+library(tidymodels)
+install.packages("palmerpenguins")
+library(palmerpenguins)
+
+# Part C
+set.seed(69)
+random_numbers <- sample(1:100, 69)
+print(random_numbers)
+
+# Part D&E
+install.packages("rsample")
+library(rsample)
+
+data_split <- initial_split(penguins, prop = 0.7)
+train_data <- training(data_split)
+test_data <- testing(data_split)
+
+# Part F
+cv_split <- vfold_cv(train_data, v = 10)
